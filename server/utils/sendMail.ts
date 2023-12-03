9e36f80341b0c907585d0f35a34ff24871d73758
Kaleab Fekadu <90934139+KaleabFekadu@users.noreply.github.com>
2023-12-03 22:11:58 +0300

    Update sendMail.ts

--- server/utils/sendMail.ts
+++ server/utils/sendMail.ts
@@ -11,6 +11,16 @@ interface EmailOptions {
 
 const sendMail = async (options: EmailOptions): Promise<void> => {
   const transporter: Transporter = nodemailer.createTransport({
-    host: process,
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    service: process.env.SMTP_SERVICE,
+    auth: {
+      user: process.env.SMTP_MAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
   });
+  const { email, subject, template, data } = options;
+
+  //get the pdath to the email template file
+  const templatePath = path.join(__dirname, "../mails", template);
 };
