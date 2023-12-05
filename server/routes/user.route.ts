39dee39ce1bc029ec01c8273e09c487aca446f90
Kaleab Fekadu <90934139+KaleabFekadu@users.noreply.github.com>
2023-12-05 21:05:04 +0300

    Update user.route.ts

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -14,6 +14,6 @@ userRouter.post("/activate-user", activateUser);
 
 userRouter.post("/login", loginUser);
 
-userRouter.get("/logout", logoutUser);
+userRouter.get("/logout", isAutheticated, logoutUser);
 
 export default userRouter;
