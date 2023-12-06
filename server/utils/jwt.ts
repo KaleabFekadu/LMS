7f9fa171ce71ebe3d4f0f074d1115144f22ff440
Kaleab Fekadu <90934139+KaleabFekadu@users.noreply.github.com>
2023-12-06 17:59:37 +0300

    everything work here

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -46,8 +46,8 @@ export const sendToken = (user: IUser, statusCode: number, res: Response) => {
     accessTokenOptions.secure = true;
   }
 
-  res.cookie("accessToken", accessToken, accessTokenOptions);
-  res.cookie("refreshToken", refreshToken, refreshTokenOptions);
+  res.cookie("access_token", accessToken, accessTokenOptions);
+  res.cookie("refresh_token", refreshToken, refreshTokenOptions);
   res.status(statusCode).json({
     success: true,
     user,
