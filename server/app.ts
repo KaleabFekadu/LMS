e201ab2ab7f539933aa9d3497154bafeafeee82b
Kaleab Fekadu <90934139+KaleabFekadu@users.noreply.github.com>
2023-12-08 01:28:10 +0300

    create order with bug

--- server/app.ts
+++ server/app.ts
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import { ErrorMiddleware } from "./middleware/error";
 import userRouter from "./routes/user.route";
 import courseRouter from "./routes/course.route";
+import orderRouter from "./routes/order.route";
 
 //body parser
 app.use(express.json({ limit: "50mb" }));
@@ -22,6 +23,7 @@ app.use(
 //routers
 app.use("/api/v1", userRouter);
 app.use("/api/v1", courseRouter);
+app.use("/api/v1", orderRouter);
 
 //tesing api
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
