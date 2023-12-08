70adc4bd271a2477d83f1f757fecd40953db1b9b
Kaleab Fekadu <90934139+KaleabFekadu@users.noreply.github.com>
2023-12-08 13:22:55 +0300

    Delete notification after 30 days admin
    still purchased not working

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -2,6 +2,7 @@ import NotificationModel from "../models/notificationModel";
 import { NextFunction, Request, Response } from "express";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import ErrorHandler from "../utils/ErrorHandler";
+import cron from "node-cron";
 
 //get all notifications -- only admin
 export const getNotifications = CatchAsyncError(
@@ -41,3 +42,13 @@ export const updateNotification = CatchAsyncError(
     }
   }
 );
+
+//delete notification ---only admin
+cron.schedule("0 0 0 * * *", async () => {
+  const thirthyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+  await NotificationModel.deleteMany({
+    status: "read",
+    createdAt: { $lt: thirthyDaysAgo },
+  });
+  console.log("Read Notifications Deleted");
+});
